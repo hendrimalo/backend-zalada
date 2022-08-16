7ea9c142e7301738162f88b456626d1c9a7e8443
hendrimalo <hendri.malo@yahoo.co.id>
2022-08-16 12:30:40 +0700

    add-search

--- controllers/products/getAll.js
+++ controllers/products/getAll.js
@@ -1,7 +1,20 @@
+const { Op } = require('sequelize');
 const { Product } = require('../../models');
 
 module.exports = async (req, res) => {
-  const products = await Product.findAll();
+  const names = req.query.names || [];
+
+  const sqlOPtions = {
+    attributes: ['id', 'name', 'purchasePrice', 'sellingPrice', 'ImageUrl', 'userId'],
+  };
+
+  if (names.length) {
+    sqlOPtions.where = {
+      name: { [Op.substring]: names },
+    };
+  }
+
+  const products = await Product.findAll(sqlOPtions);
 
   if (products.length === 0) {
     return res.status(200).json({
